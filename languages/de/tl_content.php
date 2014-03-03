62c3fdc5cb2765965ab9abe73f2a67bde6786fc5
Christoph Wiechert <wio@psitrax.de>
2014-03-03 15:52:26 +0100

    Some language corrections

--- languages/de/tl_content.php
+++ languages/de/tl_content.php
@@ -29,24 +29,16 @@ $GLOBALS['TL_LANG']['tl_content']['lightbox4ward_size']['0'] = "Lightbox-Größe
 $GLOBALS['TL_LANG']['tl_content']['lightbox4ward_size']['1'] = "Hier können Sie die Breite und Höhe der Lightbox in Pixeln festlegen.";
 $GLOBALS['TL_LANG']['tl_content']['lightbox4ward_singleSRC']['0'] = "Bild";
 $GLOBALS['TL_LANG']['tl_content']['lightbox4ward_singleSRC']['1'] = "Dieses Bild wird in der Lightbox dargestellt.";
-$GLOBALS['TL_LANG']['tl_content']['lightbox4ward_gallerySRC']['0'] = "Galeriebilder";
-$GLOBALS['TL_LANG']['tl_content']['lightbox4ward_gallerySRC']['1'] = "Wählen Sie hier die Bilder für die Galerie aus.";
 $GLOBALS['TL_LANG']['tl_content']['lightbox4ward_externURL']['0'] = "Lightbox-URL";
 $GLOBALS['TL_LANG']['tl_content']['lightbox4ward_externURL']['1'] = "Die URL zum entfernten Objekt. Dies kann ein Bild, ein social-vido-link oder eine normale Website sein.";
-$GLOBALS['TL_LANG']['tl_content']['lightbox4ward_flvSRC']['0'] = "FLV/MOV/SWF/MP4/F4V Video";
-$GLOBALS['TL_LANG']['tl_content']['lightbox4ward_flvSRC']['1'] = "Wählen Sie hier das FLV,MOV,MP4,F4V oder SWF Video aus.";
-$GLOBALS['TL_LANG']['tl_content']['lightbox4ward_html5videoSRC']['0'] = "HTML5 Video Dateien";
-$GLOBALS['TL_LANG']['tl_content']['lightbox4ward_html5videoSRC']['1'] = "Wählen Sie hier die MP4,WebM und OGV Datei des Videos aus um die größtmögliche Browserunterstützung zu erhalten.";
 $GLOBALS['TL_LANG']['tl_content']['lightbox4ward_closeOnEnd']['0'] = "Am Ende des Videos die Lightbox schließen";
 $GLOBALS['TL_LANG']['tl_content']['lightbox4ward_closeOnEnd']['1'] = "Schließt die Lightbox wenn das Video durchgelaufen ist.";
-$GLOBALS['TL_LANG']['tl_content']['lightbox4ward_mp3SRC']['0'] = "MP3/AAC Audio";
-$GLOBALS['TL_LANG']['tl_content']['lightbox4ward_mp3SRC']['1'] = "Wählen Sie hier das Audio File aus.";
 
 $GLOBALS['TL_LANG']['tl_content']['lightbox4ward_types']['image'] = 'Bild';
 $GLOBALS['TL_LANG']['tl_content']['lightbox4ward_types']['gallery'] = 'Galerie';
 $GLOBALS['TL_LANG']['tl_content']['lightbox4ward_types']['media'] = 'Media';
 $GLOBALS['TL_LANG']['tl_content']['lightbox4ward_types']['article'] = 'Artikel';
-$GLOBALS['TL_LANG']['tl_content']['lightbox4ward_types']['extern'] = 'Externe Seite';
+$GLOBALS['TL_LANG']['tl_content']['lightbox4ward_types']['extern'] = 'Externe Seite / Youtube';
 
 /**
  * ExternURL Explanation

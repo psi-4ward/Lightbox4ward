84b1569ad56a38fb1fb4f511d34db956bf28ec46
Christoph Wiechert <wio@psitrax.de>
2014-03-03 15:46:46 +0100

    Added: readme and composer.json

---
+++ README.md
@@ -0,0 +1,13 @@
+# HEADLINE
+
+desc
+
+
+Just copy the files to `system/modules/BgSlider` and run a database-update. Or use the Contao extension repository.
+
+After installation you could create a new Module `BgSlider` and include it into your pagelayout.
+
+If you use the Navigation youve to CSS `#BgSliderPrev` and `#BgSliderNext` Elements a bit. They get injected into `<body>`.
+
+License: http://www.gnu.org/licenses/lgpl-3.0.html LGPL <br>
+Author: [4ward.media](http://www.4wardmedia.de)
---
+++ languages/de/tl_settings.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * lightbox4ward
+ *
+ * A lightbox implementation for contao
+ * based on fancybox from http://fancyapps.com/fancybox
+ *
+ * @link       https://github.com/psi-4ward/lightbox4ward
+ * @copyright  4ward.media 2014 <http://www.4wardmedia.de>
+ * @author     Christoph Wiechert <wio@psitrax.de>
+ * @package    lightbox4ward
+ * @license    LGPL
+ * @filesource
+ */
+
+
+$GLOBALS['TL_LANG']['CTE']['lightbox4ward']['0'] = "Lightbox4ward";
+$GLOBALS['TL_LANG']['CTE']['lightbox4ward']['1'] = "Mit der Lightbox4ward können verschiedenste Elemente wie Videos, Galerien oder Artikel in einer Lightbox dargestellt werden.";
+ 
+$GLOBALS['TL_LANG']['tl_layout']['lightbox4ward_error'] = "Das jQuery Template j_colorbox kann nicht gleichzeitig mit j_lightbox4ward verwendet werden.";
+$GLOBALS['TL_LANG']['lightbox4ward']['noFlashMessage'] 	= '<div id="mbError"><b>Fehler<\/b><br/>Adobe Flash ist entweder nicht installiert oder zu alt, bitte besuchen Sie <a href="http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash" title="Get Flash" target="_new">Adobe.com<\/a> um eine aktuelle Version des Flash-Players herunter zu laden.<\/div>';
+$GLOBALS['TL_LANG']['lightbox4ward']['closeLbl'] 		= 'Fenster schließen';
+$GLOBALS['TL_LANG']['lightbox4ward']['nextLbl'] 		= 'nächstes Bild';
+$GLOBALS['TL_LANG']['lightbox4ward']['prevLbl'] 		= 'vorheriges Bild';
